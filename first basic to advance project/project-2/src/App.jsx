768ae81a261461e052007373a8e4486e911dfe92
udud0099 <udud0099@gmail.com>
2024-11-22 23:11:34 +0545

    project 2 onthe way

--- first basic to advance project/project-2/src/App.jsx
+++ first basic to advance project/project-2/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Hero from "./componontes/Hero/Hero";
 import Navbar from "./componontes/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />s
     </>
   );
 }
