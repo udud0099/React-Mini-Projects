479b4ab7ba63f1a43500c800db42c33c9e7ac7b0
udud0099 <udud0099@gmail.com>
2024-11-24 17:05:05 +0545

    v10 - 36m:24 - useEffect hook start

--- Chai aur react - with projects/v10-useEffect, useRef and useCallback - make password generator/src/App.jsx
+++ Chai aur react - with projects/v10-useEffect, useRef and useCallback - make password generator/src/App.jsx
@@ -10,12 +10,62 @@ function App() {
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
-    let str = "";
-  }, [length, numberAllowed, charAllowed, setPassword]);
+    let str = "ASDFGHJKLQWERTYUIOPZXCVBNMasdfghjklqwertyuiopzxcvbnm";
+    // only add A-Z and a - z
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()_+{}";
 
+    for (let i = 1; i <= length; i++) {
+      let char = Math.ceil(Math.random() * str.length);
+      pass = str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+  // console.log(password);
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {password}
+      <div>
+        <div>
+          <input type="text" value={password} placeholder="password" readOnly />
+          <button>Copy</button>
+        </div>
+        <div>
+          <input
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+          />
+          <label>Length {length}</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label>Number</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label>setCharAllowed</label>
+        </div>
+      </div>
     </>
   );
 }
